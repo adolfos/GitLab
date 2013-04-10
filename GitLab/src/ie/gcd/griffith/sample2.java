0140a23df9bf8990f6e37b6889a5d47ff56d9886
Adolfo <mac.adolfo@gmail.com>
2013-04-10 10:46:23 +0100

    Working on branch fixes

---
+++ GitLab/src/ie/gcd/griffith/sample2.java
@@ -0,0 +1,17 @@
+package ie.gcd.griffith;
+
+public class sample2 {
+
+	public sample2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
